d77b918f42f2e60cbe85b2947b4372c5f23acaaa
Zonghuang Bao <138004010+theBaoo@users.noreply.github.com>
2026-08-16 21:27:32 +0800

    Step 2: split_prng_key

--- model.py
+++ model.py
@@ -15,8 +15,12 @@ def make_prng_key(seed):
     # TODO: wrap a Python integer seed into a JAX PRNG key (uint32 array of shape (2,))
     return jax.random.PRNGKey(seed)
 
-# Step 2 - split_prng_key (not yet solved)
-# TODO: implement
+# Step 2 - split_prng_key
+import jax
+
+def split_prng_key(key, num):
+    # TODO: split `key` into `num` independent subkeys and return them as a (num, 2) array.
+    return jax.random.split(key, num)
 
 # Step 3 - sample_normal_matrix (not yet solved)
 # TODO: implement
